3e6ded169fd95e10067a3ebde3b644ab321aa60f
Livia <128159882+liviaa06@users.noreply.github.com>
2024-02-27 11:06:28 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+//var altura = 5;
+//var comprimento = 7;
+
+//area = altura * comprimento;
+//console.log(area)
+//var area;
+
+let forma = 'retângulo';
+let altura = 3;
+let comprimento = 5;
+let area;
+
+if (forma === 'retângulo') {
+    area = altura * comprimento;
+} else {
+    area = (altura * comprimento) / 2;
+}
+
+console.log(area)
+
